348ff76128dfaaea8a5c1ce1204a98e7d3070324
czy <chenzy@zlsoft.cn>
2025-07-22 11:28:31 +0800

    1

--- 1、基础数据结构/1.1、数组与字符串.py
+++ 1、基础数据结构/1.1、数组与字符串.py
@@ -1,3 +1,4 @@
+# 动态数组实现
 class DynamicArray:
     """
     动态数组实现，支持自动扩容和缩容
@@ -39,7 +40,7 @@ class DynamicArray:
         
     def insert(self, index, value):
         """在指定位置插入元素"""
-        if not 0 <= index <self.size:
+        if not 0 <= index <= self.size:
             raise IndexError(f"Index {index} out of range [0, {self.size})")
         
         # 容量检查
@@ -52,6 +53,7 @@ class DynamicArray:
             
         self.data[index-1] = value
         self.size += 1
+   
     def remove(self, index):
         """移除指定位置的元素"""
         if not 0 <= index < self.size:
@@ -84,3 +86,84 @@ class DynamicArray:
     def __str__(self):
         """字符串表示"""
         return str([self.data[i] for i in range(self.size)])
+    
+# 字符串算法基础
+class StringAlgorithms:
+    """字符串基础算法集合"""
+    
+    @staticmethod
+    def reverse_string(s):
+        """
+        反转字符串 - 多种实现方式
+        时间复杂度：O(n)
+        空间复杂度：O(n)
+        """
+        # 方法1：切片
+        # return s[::-1]
+        
+        # 方法2：双指针原地反转（如果是字符数组）
+        chars = list(s)
+        left,right = 0, len(chars) - 1
+        while left < right :
+            chars[left], chars[right] = chars[right], chars[left]
+            left += 1
+            right -=1
+            
+        return ''.join(chars)
+    
+    @staticmethod
+    def is_palindrome(s:str):
+        """
+        判断是否为回文串
+        时间复杂度：O(n)
+        空间复杂度：O(1)
+        """        
+        # 预处理：只保留数字和字母，转化为小写
+        cleaned = ''.join(c.lower() for c in s if c.isalnum())
+        
+        # 双指针判断
+        left, right = 0, len(s)-1,
+        while left < right:
+            if cleaned[left] != cleaned[right]:
+                return False
+            left += 1
+            right -=1
+        return True
+    
+    @staticmethod
+    def longest_common_prefix(strs):
+        """
+        最长公共前缀
+        时间复杂度：O(n*m) n为字符串数量，m为最短字符串长度
+        空间复杂度：O(1)
+        """
+        if not strs:
+            return ""
+        
+        # 纵向扫描法
+        for i in range(len(strs[0])):
+            char = strs[0][i]
+            for j in range(1, len(strs)):
+                if i >= len(strs[j]) or char != str[j][i]:
+                    return strs[0][:i]
+        
+        return strs[0]
+    
+    @staticmethod
+    def group_anagrams(strs):
+        """
+        字母异位词分组
+        时间复杂度：O(n*k*log(k)) n为字符串数量，k为字符串平均长度
+        空间复杂度：O(n*k)
+        """
+        from collections import defaultdict
+        
+        #  使用排序后的字符串作为key
+        anagram_map = defaultdict(list)
+        
+        for s in strs:
+            # 将字符串排序作为key
+            key = ''.join(sorted(s))
+            anagram_map[key].append(s)
+            
+        return list(anagram_map.values())
